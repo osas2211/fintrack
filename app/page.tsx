f382e746be04650f0aa764910cbfbaa5de432f59
Osaretin <osaretin.frank10@gmail.com>
2025-08-07 11:44:55 +0100

    ui: add Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { Header } from "@/components/Header"
+import { Sidebar } from "@/components/Sidebar"
 import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="bg-[#FCFDFD] w-screen min-h-screen text-[#1B2528]">
+    <main className="bg-[#FCFDFD] w-screen min-h-screen text-[#1B2528] font-sans">
       <div className="max-w-[1440px] mx-auto xl:px-12 md:px-7 px-4 pb-12">
         <Header />
+        <div className="flex gap-12 py-7">
+          <Sidebar />
+        </div>
       </div>
     </main>
   )
---
+++ components/Sidebar.tsx
@@ -0,0 +1,35 @@
+"use client"
+import Link from "next/link"
+import React from "react"
+
+const routes: { path: string; label: string }[] = [
+  { path: "/", label: "Dashboard" },
+  { path: "/", label: "Transactions" },
+  { path: "/", label: "Report" },
+  { path: "/", label: "Settings" },
+]
+
+export const Sidebar = () => {
+  return (
+    <aside className="md:w-[320px]">
+      <ul>
+        {routes.map(({ path, label }, index) => {
+          return (
+            <li key={index}>
+              <Link
+                href={path}
+                className={`${
+                  index === 0
+                    ? "text-[#3A6C7B] bg-[#386776]/16"
+                    : "hover:text-[#3A6C7B] hover:bg-[#386776]/5"
+                } block px-[18px] py-2 rounded-full font-medium transition-all`}
+              >
+                <p>{label}</p>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
+}
